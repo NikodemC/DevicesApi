6adbfcff7e2cb700bc49fc69f1f65946d2bd4588
Nikodem Cabala <nikodem.cabala@ratesetter.com>
2024-01-24 16:03:21 +0100

    Added db connection and repository

---
+++ DataAccess/DeviceDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class DeviceDbContext : DbContext
+    {
+        public DeviceDbContext(DbContextOptions opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Device> Devices { get; set; }
+    }
+}
