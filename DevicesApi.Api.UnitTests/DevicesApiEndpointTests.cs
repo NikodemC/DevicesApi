9c352a2f6c0ff07fd3175fe0b4805989d10bba41
Nikodem Cabala <nikodem.cabala@ratesetter.com>
2024-01-24 20:22:02 +0100

    Improved Api tests

--- DevicesApi.Api.UnitTests/DevicesApiEndpointTests.cs
+++ DevicesApi.Api.UnitTests/DevicesApiEndpointTests.cs
@@ -1,40 +1,45 @@
-﻿using Application.Devices.Queries;
+﻿using Application.Devices.Commands;
+using Application.Devices.Queries;
+using AutoFixture.Xunit2;
+using DevicesApi.Api.UnitTests.Helpers;
 using DevicesApi.Dtos;
+using Domain.Models;
 using FluentAssertions;
 using MediatR;
-using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
 using NSubstitute;
 using System.Net;
 using System.Net.Http.Json;
-using Domain.Models;
-using Microsoft.Extensions.DependencyInjection;
 using Xunit;
-using Application.Devices.Commands;
-using AutoFixture.Xunit2;
-using DevicesApi.Api.UnitTests.Helpers;
 
 namespace DevicesApi.Api.UnitTests
 {
     public class DevicesApiEndpointTests : IClassFixture<InMemoryDbApplication>
     {
         private readonly InMemoryDbApplication _factory;
+        private readonly IMediator _mediator;
 
         public DevicesApiEndpointTests(InMemoryDbApplication factory)
         {
             _factory = factory;
+            _mediator = Substitute.For<IMediator>();
+        }
+
+        private HttpClient CreateClientWithMediator()
+        {
+            return _factory.WithWebHostBuilder(builder => builder.ConfigureTestServices(services =>
+            {
+                services.AddSingleton(_mediator);
+            })).CreateClient();
         }
 
         [Fact]
         public async Task GetDeviceById_ValidId_ReturnsOk()
         {
             // Arrange
-            var mediator = Substitute.For<IMediator>();
-            mediator.Send(Arg.Any<GetDeviceById>()).Returns(new Device() {Id = 1, Name="Name", Brand="Brand"});
-            var client = _factory.WithWebHostBuilder(builder => builder.ConfigureTestServices(services =>
-            {
-                services.AddSingleton(mediator);
-            })).CreateClient();
+            _mediator.Send(Arg.Any<GetDeviceById>()).Returns(new Device { Id = 1, Name = "Name", Brand = "Brand" });
+            var client = CreateClientWithMediator();
 
             // Act
             var response = await client.GetAsync("/api/devices/1");
@@ -47,12 +52,8 @@ namespace DevicesApi.Api.UnitTests
         public async Task GetDeviceById_NotValidId_ReturnsNotFound()
         {
             // Arrange
-            var mediator = Substitute.For<IMediator>();
-            mediator.Send(Arg.Any<GetDeviceById>()).Returns((Device?)null);
-            var client = _factory.WithWebHostBuilder(builder => builder.ConfigureTestServices(services =>
-            {
-                services.AddSingleton(mediator);
-            })).CreateClient();
+            _mediator.Send(Arg.Any<GetDeviceById>()).Returns((Device?)null);
+            var client = CreateClientWithMediator();
 
             // Act
             var response = await client.GetAsync("/api/devices/1");
@@ -65,12 +66,8 @@ namespace DevicesApi.Api.UnitTests
         public async Task GetAllDevices_ReturnsOk(List<Device> devices)
         {
             // Arrange
-            var mediator = Substitute.For<IMediator>();
-            mediator.Send(Arg.Any<GetAllDevices>()).Returns(devices);
-            var client = _factory.WithWebHostBuilder(builder => builder.ConfigureTestServices(services =>
-            {
-                services.AddSingleton(mediator);
-            })).CreateClient();
+            _mediator.Send(Arg.Any<GetAllDevices>()).Returns(devices);
+            var client = CreateClientWithMediator();
 
             // Act
             var response = await client.GetAsync("/api/devices");
@@ -96,12 +93,8 @@ namespace DevicesApi.Api.UnitTests
         public async Task UpdateDevice_ValidDeviceDto_ReturnsOk()
         {
             // Arrange
-            var mediator = Substitute.For<IMediator>();
-            mediator.Send(Arg.Any<UpdateDevice>()).Returns(new Device() { Id = 1, Name = "Name", Brand = "Brand" });
-            var client = _factory.WithWebHostBuilder(builder => builder.ConfigureTestServices(services =>
-            {
-                services.AddSingleton(mediator);
-            })).CreateClient();
+            _mediator.Send(Arg.Any<UpdateDevice>()).Returns(new Device { Id = 1, Name = "Name", Brand = "Brand" });
+            var client = CreateClientWithMediator();
 
             // Act
             var response = await client.PutAsJsonAsync("/api/devices/1", new DeviceDto("TestName", "TestBrand"));
@@ -114,12 +107,8 @@ namespace DevicesApi.Api.UnitTests
         public async Task UpdateDevice_NoDevice_ReturnsNotFound()
         {
             // Arrange
-            var mediator = Substitute.For<IMediator>();
-            mediator.Send(Arg.Any<UpdateDevice>()).Returns((Device?)null);
-            var client = _factory.WithWebHostBuilder(builder => builder.ConfigureTestServices(services =>
-            {
-                services.AddSingleton(mediator);
-            })).CreateClient();
+            _mediator.Send(Arg.Any<UpdateDevice>()).Returns((Device?)null);
+            var client = CreateClientWithMediator();
 
             // Act
             var response = await client.PutAsJsonAsync("/api/devices/1", new DeviceDto("TestName", "TestBrand"));
