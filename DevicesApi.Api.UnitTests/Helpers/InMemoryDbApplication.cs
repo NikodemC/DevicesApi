5e8d7575f30fd2416dff28d97b0b7c54bccc9f2f
Nikodem Cabala <nikodem.cabala@ratesetter.com>
2024-01-24 19:34:06 +0100

    Added Api unit tests

---
+++ DevicesApi.Api.UnitTests/Helpers/InMemoryDbApplication.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+
+namespace DevicesApi.Api.UnitTests.Helpers;
+
+public class InMemoryDbApplication : WebApplicationFactory<Program>
+{
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureServices(collection =>
+        {
+            collection.RemoveDbContext().AddInMemorySql();
+        });
+
+        return base.CreateHost(builder);
+    }
+}
