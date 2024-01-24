2ee13d85af324fece1f447c3c3d5b8db70552f44
Nikodem Cabala <nikodem.cabala@ratesetter.com>
2024-01-24 15:22:12 +0100

    Added project structure and domain model

---
+++ Domain/Models/Device.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Models
+{
+    public class Device
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
